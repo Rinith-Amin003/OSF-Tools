2b466f0d00fa7f8fa966ec5d12a8f73bd68e71c6
vikram-N68 <vikram.narayanappa@apexit.com>
2022-11-23 15:15:29 +0530

    type isue fixed

--- src/app/pages/countries.jsx
+++ src/app/pages/countries.jsx
@@ -1,11 +1,11 @@
 import AddCountries from '../components/countryRegion/addCountries';
 import DeleteCountries from '../components/countryRegion/deleteCountries';
 import GetCountries from '../components/countryRegion/getCountries';
-import UploadCountries from '../components/countryRegion/updateCountries';
+import UpdateCountries from '../components/countryRegion/updateCountries';
 
 export {
     AddCountries as create,
     DeleteCountries as delete,
     GetCountries as list,
-    UploadCountries as upload
+    UpdateCountries as update
 }
